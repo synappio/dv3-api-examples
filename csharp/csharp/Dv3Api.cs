a73b610fdac7a2fcb8dc2385bbb373fa2c7b0d03
Attila-Mihály Balázs (Grey Panther) <attila@synapp.io>
2017-04-03 14:21:03 +0300

    Add initial examples in C#

---
+++ csharp/csharp/Dv3Api.cs
@@ -0,0 +1,75 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net;
+
+namespace csharp
+{
+    sealed class Dv3Api
+    {
+        public enum Grade { A_PLUS, A, B, D, F }
+
+        private const string API_ROOT = "https://dv3.datavalidation.com/api/v2";
+
+        private readonly string apiKey;
+
+        public Dv3Api(string apiKey)
+        {
+            this.apiKey = apiKey;
+        }
+
+        public Grade Realtime(String email)
+        {
+            dynamic m = this.AuthenticatedJsonRequest("/realtime/?email=" + WebUtility.UrlEncode(email));
+            if (m.status != "ok")
+            {
+                throw new WebException("Invalid response status: " + m.status);
+            }
+            return From((string)m.grade);
+        }
+
+        public string GetUploadUrl(string listName, int emailColumnIndex, bool firstLineIsHeader)
+        {
+            return this.AuthenticatedJsonRequest(
+                "/user/me/list/create_upload_url/?name=" + WebUtility.UrlEncode(listName)
+                + "&email_column_index=" + emailColumnIndex + "&has_header=" + firstLineIsHeader);
+        }
+
+        public string Upload(string uploadUrl, string filePath)
+        {
+            using (var client = new WebClient())
+            {
+                client.Headers.Add("Authorization: bearer " + this.apiKey);
+                byte[] response = client.UploadFile(uploadUrl, filePath);
+                return (string)JsonConvert.DeserializeObject(System.Text.Encoding.UTF8.GetString(response));
+            }
+        }
+
+        public dynamic ListInfo(string listId)
+        {
+            return this.AuthenticatedJsonRequest("/user/me/list/" + WebUtility.UrlEncode(listId) + "/");
+        }
+
+        private Grade From(String value)
+        {
+            switch (value)
+            {
+                case "A+": return Grade.A_PLUS;
+                case "A": return Grade.A;
+                case "B": return Grade.B;
+                case "D": return Grade.D;
+                case "F": return Grade.F;
+                default:
+                    throw new System.ArgumentException("Invalid grade: " + value);
+            }
+        }
+
+        private dynamic AuthenticatedJsonRequest(string urlSuffix)
+        {
+            using (var client = new WebClient())
+            {
+                client.Headers.Add("Authorization: bearer " + this.apiKey);
+                return JsonConvert.DeserializeObject(client.DownloadString(API_ROOT + urlSuffix));
+            }
+        }
+    }
+}
---
+++ csharp/csharp/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string operation = args[0];
+            string apiKey = args[1];
+
+            Dv3Api apiClient = new Dv3Api(apiKey);
+            switch (operation)
+            {
+                case "realtime":
+                    RealtimeValidation(apiClient, args[2]);
+                    break;
+                case "list":
+                    CheckList(apiClient, args[2], args[3]);
+                    break;
+                default:
+                    throw new System.ArgumentException("Invalid operation: " + operation);
+            }
+        }
+
+        private static void RealtimeValidation(Dv3Api apiClient, string email)
+        {
+            Console.WriteLine("{0} got a(n) {1}.", email, apiClient.Realtime(email));
+        }
+
+        private static void CheckList(Dv3Api apiClient, string listName, string fileName)
+        {
+            Console.WriteLine("Getting upload URL...");
+            string uploadUrl = apiClient.GetUploadUrl(listName, 0, false);
+            Console.WriteLine("Upload URL: {0}", uploadUrl);
+            Console.WriteLine("Uploading file contents");
+            string listId = apiClient.Upload(uploadUrl, fileName);
+            Console.WriteLine("List created. List id: {0}", listId);
+
+            while (true)
+            {
+                dynamic info = apiClient.ListInfo(listId);
+                if (info.status_value == "VALIDATED")
+                {
+                    Console.WriteLine("List validated. Grade summary: {0}", info.grade_summary);
+                    break;
+                }
+                else if (info.status_value == "FAILED")
+                {
+                    Console.WriteLine("List validationd failed!");
+                    break;
+                }
+
+                Console.WriteLine("Status: {0} ({1} %)", info.status_value, info.status_percent_complete);
+                System.Threading.Thread.Sleep(TimeSpan.FromSeconds(5));
+            }
+        }
+    }
+}
